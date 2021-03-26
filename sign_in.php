6e8c47f20c401329b4384532f540b53181969201
Md.zakir al mahfuj <mmahfuj161208@bscse.uiu.ac.bd>
2021-03-27 04:41:27 +0600

    file create

---
+++ sign_in.php
@@ -0,0 +1,10 @@
+
+
+<form action="../api/post/api_sign_in.php" method="post">
+  <label >email:</label><br>
+  <input type="text" name="email"><br>
+  <label >pass:</label><br>
+  <input type="password"  name="pass"><br>
+  
+  <input type="submit" >
+</form>
