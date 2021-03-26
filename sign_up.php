65b46e9ebc7e86f11d526ef627cda759867a7381
Md.zakir al mahfuj <mmahfuj161208@bscse.uiu.ac.bd>
2021-03-27 04:49:08 +0600

    css file edit

---
+++ sign_up.php
@@ -0,0 +1,13 @@
+
+
+<form action="../api/post/api_sign_up.php" method="post">
+  <label >email:</label><br>
+  <input type="text" name="email"><br>
+  <label >pass:</label><br>
+  <input type="password"  name="pass"><br>
+  <label >name:</label><br>
+  <input type="text"  name="name">
+  <br>
+  
+  <input type="submit" >
+</form>
